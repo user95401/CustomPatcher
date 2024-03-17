fcca16a8f4632c07488ef7f957889bdb2bd2aa08
user95401 <user95401@gmail.com>
2024-03-17 17:14:01 +0400

    localsettingsfix

--- src/_YourLocalCfg.cpp
+++ src/_YourLocalCfg.cpp
@@ -1,35 +1,43 @@
-//geode
 #include <Geode/Geode.hpp>
 using namespace geode::prelude;
 
-void CopyFromLocal() {
-    auto game_dir = Mod::get()->getConfigDir() / "settings.json"; //from_game_dir
-    auto save_dir = Mod::get()->getSaveDir() / "settings.json"; //from_game_dir
-    if (!ghc::filesystem::exists(game_dir)) return;
-    //if (!ghc::filesystem::exists(save_dir)) return;
+auto g_game_json = Mod::get()->getConfigDir() / "settings.json";
+auto g_game_dir = Mod::get()->getConfigDir();
+
+auto g_data_json = Mod::get()->getSaveDir() / "settings.json";
+auto g_data_dir = Mod::get()->getSaveDir();
+
+void copy_settings_from_game_dir() {
+    log::info("{}", __FUNCTION__);
+    log::debug("g_game_json: {}", g_game_json);
+    log::debug("g_game_dir: {}", g_game_dir);
+    log::debug("g_data_json: {}", g_data_json);
+    log::debug("g_data_dir: {}", g_data_dir);
+    if (!std::ifstream(g_game_json.string().c_str()).good()) return log::warn("cant get the {}", g_game_json);
     ghc::filesystem::copy(
-        game_dir, //from game dir
-        save_dir, //to game save dir
+        g_game_json, //from game dir
+        g_data_dir, //to game data dir
         ghc::filesystem::copy_options::overwrite_existing);
 };
-void CopyFromData() {
-    auto game_dir = Mod::get()->getConfigDir() / "settings.json"; //from_game_dir
-    auto save_dir = Mod::get()->getSaveDir() / "settings.json"; //from_game_dir
-    //if (!ghc::filesystem::exists(game_dir)) return;
-    if (!ghc::filesystem::exists(save_dir)) return;
+
+void copy_settings_from_data_dir() {
+    log::info("{}", __FUNCTION__);
+    log::debug("g_game_json: {}", g_game_json);
+    log::debug("g_game_dir: {}", g_game_dir);
+    log::debug("g_data_json: {}", g_data_json);
+    log::debug("g_data_dir: {}", g_data_dir);
+    if (!std::ifstream(g_data_json.string().c_str()).good()) return log::warn("cant get the {}", g_data_json);
     ghc::filesystem::copy(
-        save_dir, //to game save dir
-        game_dir, //from game dir
+        g_data_json, //to game data dir
+        g_game_dir, //from game dir
         ghc::filesystem::copy_options::overwrite_existing);
 };
+
 $on_mod(Loaded) {
-    CopyFromLocal();
+    copy_settings_from_game_dir();
     Mod::get()->loadData();
 };
-#include <Geode/modify/AppDelegate.hpp>
-class $modify(AppDelegate) {
-    void trySaveGame(bool p0) {
-        AppDelegate::trySaveGame(p0);
-        CopyFromData();
-    }
+
+$on_mod(DataSaved) {
+    copy_settings_from_data_dir();
 };
